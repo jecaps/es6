73ab928d59f3a353c1365ec5be0332582dd7cacb
Jerika Kanz <jerikacapila@gmail.com>
2021-10-23 19:47:31 +0200

    Compare Scopes of the var and let Keywords

--- let.js
+++ let.js
@@ -6,3 +6,17 @@ Variables defined with let have Block Scope.
 let camper = "James";
 // let camper = "David"; cannot overwrite variable once its declared
 console.log(camper);
+
+// When let is declared inside a blockm statement, or expression, its scope is limited to that block, statement, or expression:
+
+function checkScope() {
+  let i = "function scope";
+  if (true) {
+    let i = "block scope";
+    console.log("Block scope i is: ", i);
+  }
+  console.log("Function scope i is: ", i);
+  return i;
+}
+
+console.log(checkScope());
