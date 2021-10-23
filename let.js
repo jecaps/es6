272a7c1098503eb43157a08fd401ce13dbf0fd9a
Jerika Kanz <jerikacapila@gmail.com>
2021-10-23 19:38:29 +0200

    Explore Differences Between the var and let Keywords

--- let.js
+++ let.js
@@ -1,1 +1,8 @@
+/* Variables defined with let cannot be Redeclared.
+Variables defined with let must be Declared before use.
+Variables defined with let have Block Scope.
+*/
 
+let camper = "James";
+// let camper = "David"; cannot overwrite variable once its declared
+console.log(camper);
