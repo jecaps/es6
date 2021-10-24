80625ad4569d07cdcbcd2194b1570a5b00f50722
Jerika Kanz <jerikacapila@gmail.com>
2021-10-24 19:32:08 +0200

    Write Arrow Functions with Parameters

--- arrow.js
+++ arrow.js
@@ -2,14 +2,25 @@
   Arrow function expression is a compact alternative to a traditional function expression, but is limited and can't be used in all situations.
  */
 
-const myFunc = function () {
+/*const myFunc = function () {
   const myVar = "value";
   return myVar;
-}; //This function...
+}; //This function...*/
 
-const myFunc = () => {
+/*const myFunc = () => {
   const myVar = "value";
   return myVar;
-}; //... can also be written like this...
+}; //... can also be written like this...*/
 
 const myFunc = () => "value"; //If the function has only one statement, and the statement returns a value, you can remove the brackets and the return keyword.
+
+// Just like a regular function,  you can pass argument into an arrow function.
+
+const doubler = (item) => item * 2;
+doubler(4); // If an arrow function has a single parameter, the parentheses enclosing the parameter may be omitted.
+
+const multiplier = (item, multi) => item * multi;
+multiplier(4, 2); //It is possible to pass more than one argument into an arrow function.
+
+const myConcat = (arr1, arr2) => arr1.concat(arr2);
+console.log(myConcat([1, 2], [3, 4, 5]));
