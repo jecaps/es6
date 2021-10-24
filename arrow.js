88a123763499ab99a0371a858567d95cc4e94505
Jerika Kanz <jerikacapila@gmail.com>
2021-10-24 19:54:20 +0200

    Use the Rest Parameter with Function Parameters

--- arrow.js
+++ arrow.js
@@ -32,3 +32,18 @@ console.log(greeting("John"));
 console.log(greeting());
 
 const increment = (number, value = 1) => number + value;
+
+/* In order to help us create more flexible functions, ES6 introduces the rest parameter for function parameters. 
+  With the rest parameter, you can create functions that take a variable number of arguments. 
+  These arguments are stored in an array that can be accessed later from inside the function. */
+
+function howMany(...args) {
+  return "You have passed " + args.length + " arguments.";
+}
+console.log(howMany(0, 1, 2));
+console.log(howMany("string", null, [1, 2, 3], {}));
+
+const sum = (...args) => {
+  return args.reduce((a, b) => a + b, 0);
+};
+console.log(sum(3, 6, 7));
