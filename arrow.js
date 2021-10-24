8d40d716755ff462ad8d35b47e294f5c765ca681
Jerika Kanz <jerikacapila@gmail.com>
2021-10-24 19:41:38 +0200

    Set Default Parameters for Your Functions

--- arrow.js
+++ arrow.js
@@ -24,3 +24,11 @@ multiplier(4, 2); //It is possible to pass more than one argument into an arrow 
 
 const myConcat = (arr1, arr2) => arr1.concat(arr2);
 console.log(myConcat([1, 2], [3, 4, 5]));
+
+// In order to help us create more flexible functions, ES6 introduces default parameters for functions.
+
+const greeting = (name = "Anonymous") => "Hello " + name;
+console.log(greeting("John"));
+console.log(greeting());
+
+const increment = (number, value = 1) => number + value;
